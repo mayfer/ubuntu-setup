9decee2ff7b628d88ca2a76d698e2ab026584f3a
Murat Ayfer <murat@ayfer.net>
2026-01-16 15:24:07 -0800

    lock

--- prevent_sleep.sh
+++ prevent_sleep.sh
@@ -1,3 +1,6 @@
 # Disable automatic suspend
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+
+# Disable lock that asks for password
+gsettings set org.gnome.desktop.screensaver lock-enabled false
